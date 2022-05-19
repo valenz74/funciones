8af6e920cc42e1a0948734c2683121e6bbc47db9
Jose Valenzuela <valenz74@gmail.com>
2022-05-19 18:25:52 -0500

    Aprendiendo Funciones en Java

---
+++ src/Funciones.java
@@ -0,0 +1,12 @@
+public class Funciones {
+    public static void main(String[] args) {
+       var suma = sumar(10, 27);
+        System.out.println("La suma es: " + suma);
+    }
+
+    static int sumar(int num1, int num2) {
+        return (num1 + num2);
+    }
+
+
+}
